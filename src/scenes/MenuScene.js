a3f06679f9a55417035efdd6614ebc2c87c608e7
Mario Retana <maretanar@gmail.com>
2019-02-05 19:13:38 -0600

    Added menu text

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -1,12 +1,30 @@
 import Phaser from 'phaser'
 
 export default class MenuScene extends Phaser.Scene {
+  static MENU_OPTION = Object.freeze({
+    PLAY: 0,
+    CREDITS: 1
+  })
+
   constructor () {
     super({ key: 'MENU_SCENE' })
   }
+
+  addMenuText () {
+    let centerX = this.sys.game.config.width / 2
+    this.add.bitmapText(centerX, 16, 'unibody', 'ARKANOID', 64).setOrigin(0.5, 0)
+    this.add.bitmapText(centerX, 90, 'unibody', 'a game made with Phaser 3', 12).setOrigin(0.5, 0)
+    this.add.bitmapText(centerX, 152, 'unibody', 'PLAY', 30).setOrigin(0.5, 0)
+    this.add.bitmapText(centerX, 192, 'unibody', 'CREDITS', 30).setOrigin(0.5, 0)
+    this.add.bitmapText(centerX, 272, 'unibody', 'Instructions', 14).setOrigin(0.5, 0)
+    this.add.bitmapText(centerX, 294, 'unibody', 'use arrow keys to move - spacebar to select and shoot', 14).setOrigin(0.5, 0)
+    this.add.bitmapText(this.sys.game.config.width - 8, this.sys.game.config.height - 8, 'unibody', 'Mario Retana - 2019', 14).setOrigin(1, 1)
+  }
+
   preload () {}
   create () {
-    this.add.bitmapText(16, 16, 'unibody', 'This is the menu scene', 16)
+    window.debugThis = this
+    this.addMenuText()
   }
   update () {}
 }
