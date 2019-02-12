662768877d0f3df7d6c7a0bf92330d8e073cf625
Mario Retana <maretanar@gmail.com>
2019-02-12 00:59:43 -0600

    Added game base assets

--- src/scenes/LoadScene.js
+++ src/scenes/LoadScene.js
@@ -7,6 +7,7 @@ export default class LoadScene extends Phaser.Scene {
   }
   preload () {
     this.load.bitmapFont(CONSTS.FONT.DEFAULT, 'assets/font/unibody.png', 'assets/font/unibody.fnt')
+    this.load.atlas('spritesheet', 'assets/atlas/spritesheet.png', 'assets/atlas/spritesheet.json')
   }
   create () {
     this.scene.start(CONSTS.SCENE.MENU)
